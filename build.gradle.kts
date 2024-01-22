ffcb448df8319979bc368086b452e3d0cc0eaf09
hjm <gks777777@gmail.com>
2024-01-22 19:50:45 +0900

    Add: [#issueNumber-5]implementation 위치 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0") //swager
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0") //swager
+
 }
 
 noArg {

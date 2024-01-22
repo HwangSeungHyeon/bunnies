72d884cb01bc94d0dfa014a3f25f432b56c3dfee
HwangSeungHyeon <xlddy02@gmail.com>
2024-01-22 15:28:02 +0900

    Feat: initialize project with basic structure 프로젝트 생성 후 첫번째 커밋

---
+++ src/main/kotlin/com/teamsparta/bunnies/BunniesApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.bunnies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BunniesApplication
+
+fun main(args: Array<String>) {
+    runApplication<BunniesApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/bunnies/BunniesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.bunnies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BunniesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

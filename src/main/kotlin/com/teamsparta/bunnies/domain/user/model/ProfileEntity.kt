13cf581f9dc36d1a4f184853e279046283692fdd
9nh5 <koon6756@naver.com>
2024-01-23 21:34:30 +0900

    Feat: "Add profile CRUD"
    프로필 조회, 수정, 삭제
    비밀번호 변경
    
    Ref: #4

--- src/main/kotlin/com/teamsparta/bunnies/domain/user/model/ProfileEntity.kt
+++ src/main/kotlin/com/teamsparta/bunnies/domain/user/model/ProfileEntity.kt
@@ -1,9 +1,7 @@
 package com.teamsparta.bunnies.domain.user.model
 
-import com.teamsparta.bunnies.domain.user.dto.request.UpdateUserProfileRequestDto
 import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
-import org.springframework.security.crypto.password.PasswordEncoder
 
 @Embeddable
 class ProfileEntity(
--- src/main/kotlin/com/teamsparta/bunnies/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/teamsparta/bunnies/domain/user/repository/UserRepository.kt
@@ -1,6 +1,5 @@
 package com.teamsparta.bunnies.domain.user.repository
 
-import com.teamsparta.bunnies.domain.user.model.ProfileEntity
 import com.teamsparta.bunnies.domain.user.model.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
@@ -10,7 +9,7 @@ interface UserRepository: JpaRepository <UserEntity, Long> {
 
     fun findByProfileEntityEmail(email:String) : UserEntity?
 
-    fun findByProfileEntityNickname(nickname: String): UserEntity?
+//    fun findByProfileEntityNickname(nickname: String): UserEntity?
 
 //    fun findTop3ByProfileEntityPasswordOrderBy(profileEntity: ProfileEntity):
 }

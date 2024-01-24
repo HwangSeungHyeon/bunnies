e1c6bf32acc0fa478b13fa2bdc4a54d12019fdc1
9nh5 <koon6756@naver.com>
2024-01-24 20:15:13 +0900

    Feat: "Add profile CRUD"
    프로필 조회, 수정, 삭제
    비밀번호 변경
    
    Ref: #4

--- src/main/kotlin/com/teamsparta/bunnies/domain/exception/NotAuthorizationException.kt
+++ src/main/kotlin/com/teamsparta/bunnies/domain/exception/NotAuthorizationException.kt
@@ -1,4 +1,3 @@
 package com.teamsparta.bunnies.domain.exception
 
-class NotAuthorizationException: RuntimeException ("사용자 권한이 없습니다."){
-}
+class NotAuthorizationException: RuntimeException ("사용자 권한이 없습니다.")

a12ee3db8f42058ccd33176b7b4d707f8eeb13a9
HwangSeungHyeon <xlddy02@gmail.com>
2024-01-26 11:16:44 +0900

    Fix: "fix Mixed content error"
    BunniesApplication.kt에 OpenAPIDefintion을 추가하여 에러를 해결
    
    Resolves: #28
    Ref: #9

--- src/main/kotlin/com/teamsparta/bunnies/BunniesApplication.kt
+++ src/main/kotlin/com/teamsparta/bunnies/BunniesApplication.kt
@@ -1,9 +1,12 @@
 package com.teamsparta.bunnies
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@OpenAPIDefinition(servers = [Server(url = "https://2cfa-220-92-92-230.ngrok-free.app/", description = "Default Server URL")]) //https
 @EnableJpaAuditing //AuditingEntityListener 기능 사용하기 위해서 추가
 @SpringBootApplication
 class BunniesApplication

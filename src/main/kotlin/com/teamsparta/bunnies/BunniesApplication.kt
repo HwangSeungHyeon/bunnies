8293a210c182708d01c71d774ffffb178bf1e069
HwangSeungHyeon <xlddy02@gmail.com>
2024-01-26 11:26:26 +0900

    Refactor: "edit server url"
    server url을 외부파일에서 가져오는 형식으로 수정
    
    Ref: #31

--- src/main/kotlin/com/teamsparta/bunnies/BunniesApplication.kt
+++ src/main/kotlin/com/teamsparta/bunnies/BunniesApplication.kt
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@OpenAPIDefinition(servers = [Server(url = "https://2cfa-220-92-92-230.ngrok-free.app/", description = "Default Server URL")]) //https
+@PropertySource("classpath:server.yml")
+@OpenAPIDefinition(servers = [Server(url = ("\${server}"), description = "Default Server URL")]) //https
 @EnableJpaAuditing //AuditingEntityListener 기능 사용하기 위해서 추가
 @SpringBootApplication
 class BunniesApplication
